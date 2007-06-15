3991f59184f293ce8b27d7796f4fcc30ceb0b4af
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-06-15 17:42:07 +0000

    Fixed classpath

--- bluecove-examples/bluecove-tester/run-bluesoleil.cmd
+++ bluecove-examples/bluecove-tester/run-bluesoleil.cmd
@@ -4,7 +4,7 @@ SETLOCAL
 call %~dp0scripts\version.cmd
 SET STACK=bluesoleil
 title %STACK%-tester
-java -Dbluecove.stack=%STACK% -jar bluecove-tester-%VERSION%-app.jar >  run-%STACK%.cmd.log
+java -Dbluecove.stack=%STACK% -jar target\bluecove-tester-%VERSION%-app.jar >  run-%STACK%.cmd.log
 if errorlevel 1 (
     echo Error calling java
     pause
