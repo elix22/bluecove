2f1de743eed5de0b53423042fb257d24aaf73d04
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-06-19 03:47:52 +0000

    log2file

--- bluecove-examples/bluecove-tester/run-winsock-1.2.3.cmd
+++ bluecove-examples/bluecove-tester/run-winsock-1.2.3.cmd
@@ -3,7 +3,8 @@ rem @version $Revision$ ($Author$)  $Date$
 SETLOCAL
 call %~dp0scripts\version.cmd
 
-java -classpath src\site\resources\bluecove-1.2.3-signed.jar;target\bluecove-tester-%VERSION%-app.jar net.sf.bluecove.awt.Main
+title bluecove-1.2.3-tester
+java -classpath src\site\resources\bluecove-1.2.3-signed.jar;target\bluecove-tester-%VERSION%-app.jar net.sf.bluecove.awt.Main >  run-winsock-1.2.3.cmd.log
 if errorlevel 1 (
     echo Error calling java
     pause
