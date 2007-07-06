a622744df6bdb880d8d7e610a3c4923c7def139f
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-07-06 02:12:31 +0000

    release 2.0.0

--- bluecove-examples/bluecove-tester/scripts/version.cmd
+++ bluecove-examples/bluecove-tester/scripts/version.cmd
@@ -4,6 +4,7 @@ rem @version $Revision$ ($Author$)  $Date$
 rem set JAVA_HOME=D:\jdk1.5.0
 rem set PATH=%JAVA_HOME%\bin;%PATH%
 
-set VERSION=2.0.0-SNAPSHOT
+rem set VERSION=2.0.0-SNAPSHOT
+set VERSION=2.0.0
 
 set MAVEN2_REPO=%HOMEDRIVE%\%HOMEPATH%\.m2\repository
