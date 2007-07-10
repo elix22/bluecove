3c41ff3d7f49fa7bd63f952d664107097eda3c6a
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-07-10 03:56:54 +0000

    fixed echo

--- bluecove-examples/bluecove-tester/src/main/java/net/sf/bluecove/TestResponderServer.java
+++ bluecove-examples/bluecove-tester/src/main/java/net/sf/bluecove/TestResponderServer.java
@@ -139,8 +139,8 @@ public class TestResponderServer implements CanShutdown, Runnable {
 			cBuf[cBufIdx] = firstChar;
 			cBufIdx ++;
 			try {
-				c.os.write(firstChar);
 				c.os = c.conn.openOutputStream();
+				c.os.write(firstChar);
 				OutputStream os = c.os;
 				int i;
 				while ((i = is.read()) != -1) {
