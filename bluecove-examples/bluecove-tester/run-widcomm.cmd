29939024fce6a52df22b895e8448a6b41fd55efe
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-11-09 23:49:40 +0000

    test connection timeout

--- bluecove-examples/bluecove-tester/run-widcomm.cmd
+++ bluecove-examples/bluecove-tester/run-widcomm.cmd
@@ -4,7 +4,11 @@ SETLOCAL
 call %~dp0scripts\version.cmd
 SET STACK=widcomm
 title %STACK%-tester
-java -Dbluecove.stack=%STACK% -jar target\bluecove-tester-%VERSION%-app.jar >  run-%STACK%.cmd.log
+
+set JVM_ARGS=
+rem set JVM_ARGS=%JVM_ARGS% -Dbluecove.connect.timeout=10000
+
+java %JVM_ARGS% -Dbluecove.stack=%STACK% -jar target\bluecove-tester-%VERSION%-app.jar >  run-%STACK%.cmd.log
 if errorlevel 2 (
     echo Error calling java
 )
