9b6bad828382978e645016421a39c55c3115cf21
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-11-19 22:08:43 +0000

    tests on WIDCOMM

--- bluecove-examples/bluecove-tester/run-widcomm.cmd
+++ bluecove-examples/bluecove-tester/run-widcomm.cmd
@@ -8,7 +8,14 @@ title %STACK%-tester
 set JVM_ARGS=
 rem set JVM_ARGS=%JVM_ARGS% -Dbluecove.connect.timeout=10000
 
-java %JVM_ARGS% -Dbluecove.stack=%STACK% -jar target\bluecove-tester-%VERSION%-app.jar >  run-%STACK%.cmd.log
+set BLUECOVE_PROJECT=%~dp0..\..\bluecove
+
+set CP=target\classes;%BLUECOVE_PROJECT%\target\classes;target\cldcunit.jar;target\cldcunit-se.jar
+
+set START_ARGS=-cp %CP% -Dbluecove.native.path=%BLUECOVE_PROJECT%/src/main/resources net.sf.bluecove.awt.Main
+rem set START_ARGS=-jar target\bluecove-tester-%VERSION%-app.jar
+
+java %JVM_ARGS% -Dbluecove.stack=%STACK% %START_ARGS% >  run-%STACK%.cmd.log
 if errorlevel 2 (
     echo Error calling java
 )
