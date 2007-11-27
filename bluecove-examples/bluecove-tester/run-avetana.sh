90a985c17877caac47207d9d49a1e776235a82cc
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-11-27 04:30:40 +0000

    OSX tests avetana

---
+++ bluecove-examples/bluecove-tester/run-avetana.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+#  @version $Revision$ ($Author$) $Date$
+#
+
+java -cp ./../../3p/avetanaBluetooth/avetanaBluetooth.jar:./target/cldcunit.jar:./target/cldcunit-se.jar:./target/bluecove-tester-2.0.2-SNAPSHOT.jar net.sf.bluecove.awt.Main
