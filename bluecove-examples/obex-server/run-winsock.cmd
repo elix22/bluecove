6010f8dbbcd5884b1efcdb16f1b394900d378c02
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-12-19 19:21:38 +0000

    -Dbluecove.obex.mtu=40000

--- bluecove-examples/obex-server/run-winsock.cmd
+++ bluecove-examples/obex-server/run-winsock.cmd
@@ -4,7 +4,10 @@ SETLOCAL
 call %~dp0..\bluecove-tester\scripts\version.cmd
 SET STACK=winsock
 title %STACK%-obex
+
 rem set JVM_ARGS=%JVM_ARGS% -Dbluecove.debug=1
+JVM_ARGS=%JVM_ARGS% -Dbluecove.obex.mtu=40000
+
 java -Dbluecove.stack=%STACK% %JVM_ARGS% -jar target\obex-server-%VERSION%-app.jar
 if errorlevel 1 (
     echo Error calling java
