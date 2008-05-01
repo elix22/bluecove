b8c60753406dda883a48498c5df78552909469b8
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2008-05-01 21:49:43 +0000

    Scrips 4 Linux

--- bluecove-examples/bluecove-tester/scripts/version.sh
+++ bluecove-examples/bluecove-tester/scripts/version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# @version $Revision: 1266 $ ($Author: skarzhevskyy@gmail.com $) $Date: 2007-11-28 02:39:05 -0500 (Wed, 28 Nov 2007) $
+# @version $Revision$ ($Author$) $Date$
 #
 GENERATED_VERSION="${SCRIPTS_DIR}/generated-version.sh"
 if [ ! -f ${GENERATED_VERSION} ]; then
