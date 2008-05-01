7e71f9151fb29d4da1a596d2a48d5f29b82da11c
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2008-05-01 23:36:13 +0000

    fixed midp20 start on linux

--- bluecove-examples/bluecove-tester/scripts/linux/j9-midp20_weme-6.1.1.sh
+++ bluecove-examples/bluecove-tester/scripts/linux/j9-midp20_weme-6.1.1.sh
@@ -12,7 +12,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${JAVAHOME}/bin
 export MALLOC_CHECK_=0
 
 JVM_ARGS="-jcl:midp20"
-#JVM_ARGS="${JVM_ARGS} -Xbootclasspath:%JAVAHOME%\lib\jclMidp20\jclMidp20.jxe"
+JVM_ARGS="${JVM_ARGS} -Xbootclasspath:${JAVAHOME}/lib/jclMidp20/jclMidp20.jxe"
 JVM_ARGS="${JVM_ARGS} -Dmicroedition.connection.pkgs=com.intel.bluetooth"
 #JVM_ARGS="${JVM_ARGS} -Dbluecove.debug=1"
 
@@ -21,7 +21,7 @@ cp ${BLUECOVE_GPL_JAR} "${JAVAHOME}/lib/jclMidp20/ext/bluecove-gpl.jar"
 cp ${BLUECOVE_GPL_PROJECT_HOME}/src/main/resources/*.so "${JAVA_HOME}/bin"
 
 
-${JAVAHOME}/bin/emulator ${JVM_ARGS} -cp ${BLUECOVE_TESTER_HOME}/target/bctest.jar javax.microedition.lcdui.AppManager ${BLUECOVE_TESTER_HOME}/target/bctest.jad
+${JAVAHOME}/bin/j9 ${JVM_ARGS} -cp ${BLUECOVE_TESTER_HOME}/target/bctest.jar javax.microedition.lcdui.AppManager ${BLUECOVE_TESTER_HOME}/target/bctest.jad
 rc=$?
 if [ ! "${rc}" = "0" ]; then
     echo Error calling java
