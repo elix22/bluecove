dc7fa8f3cb93578beab898c229089807a3e8262c
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2008-05-02 14:12:25 +0000

    mkdir if not exists

--- bluecove-examples/bluecove-tester/scripts/linux/j9-midp20_weme-6.1.1.sh
+++ bluecove-examples/bluecove-tester/scripts/linux/j9-midp20_weme-6.1.1.sh
@@ -16,8 +16,13 @@ JVM_ARGS="${JVM_ARGS} -Xbootclasspath:${JAVAHOME}/lib/jclMidp20/jclMidp20.jxe"
 JVM_ARGS="${JVM_ARGS} -Dmicroedition.connection.pkgs=com.intel.bluetooth"
 #JVM_ARGS="${JVM_ARGS} -Dbluecove.debug=1"
 
-cp ${BLUECOVE_JAR} "${JAVAHOME}/lib/jclMidp20/ext/bluecove.jar"
-cp ${BLUECOVE_GPL_JAR} "${JAVAHOME}/lib/jclMidp20/ext/bluecove-gpl.jar"
+JCLMIDP20_EXT_DIR="${JAVAHOME}/lib/jclMidp20/ext"
+if [ ! -d ${GENERATED_VERSION} ]; then
+    mkdir ${JCLMIDP20_EXT_DIR}
+fi
+
+cp ${BLUECOVE_JAR} "${JCLMIDP20_EXT_DIR}/bluecove.jar"
+cp ${BLUECOVE_GPL_JAR} "${JCLMIDP20_EXT_DIR}/bluecove-gpl.jar"
 cp ${BLUECOVE_GPL_PROJECT_HOME}/src/main/resources/*.so "${JAVA_HOME}/bin"
 
 
