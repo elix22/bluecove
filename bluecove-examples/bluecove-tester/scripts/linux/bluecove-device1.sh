60f7363f0944421744d4b7c0fae58592f26bf895
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2008-05-02 01:33:00 +0000

    fixed device 1

--- bluecove-examples/bluecove-tester/scripts/linux/bluecove-device1.sh
+++ bluecove-examples/bluecove-tester/scripts/linux/bluecove-device1.sh
@@ -4,7 +4,7 @@
 SCRIPTS_DIR=`dirname ${0}`/..
 . ${SCRIPTS_DIR}/environment.sh
 
-JVM_ARGS="-Dbluecove.deviceID=1
+JVM_ARGS="-Dbluecove.deviceID=1"
 #JVM_ARGS="${JVM_ARGS} -Dbluecove.debug=1"
 
 java ${JVM_ARGS} -cp ${BLUECOVE_TESTER_APP_JAR} net.sf.bluecove.awt.Main
