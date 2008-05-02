4156cf5dd0300695e2e88fa99d3268304ab0ef74
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2008-05-02 14:24:03 +0000

    mkdir if not exists

--- bluecove-examples/bluecove-tester/scripts/linux/j9-midp20_weme-6.1.1.sh
+++ bluecove-examples/bluecove-tester/scripts/linux/j9-midp20_weme-6.1.1.sh
@@ -4,20 +4,20 @@
 SCRIPTS_DIR=`dirname ${0}`/..
 . ${SCRIPTS_DIR}/environment.sh
 
-export JAVAHOME=/opt/IBM/WEME/runtimes/61/lnx-x86-midp20
-export PATH=${JAVAHOME}/bin/:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${JAVAHOME}/bin
+export JAVA_HOME=/opt/IBM/WEME/runtimes/61/lnx-x86-midp20
+export PATH=${JAVA_HOME}/bin/:${PATH}
+export LD_LIBRARY_PATH=${JAVA_HOME}/bin:${LD_LIBRARY_PATH}
 
 # igone j9 bugs *** glibc detected *** free(): invalid pointer:...
 export MALLOC_CHECK_=0
 
 JVM_ARGS="-jcl:midp20"
-JVM_ARGS="${JVM_ARGS} -Xbootclasspath:${JAVAHOME}/lib/jclMidp20/jclMidp20.jxe"
+JVM_ARGS="${JVM_ARGS} -Xbootclasspath:${JAVA_HOME}/lib/jclMidp20/jclMidp20.jxe"
 JVM_ARGS="${JVM_ARGS} -Dmicroedition.connection.pkgs=com.intel.bluetooth"
 #JVM_ARGS="${JVM_ARGS} -Dbluecove.debug=1"
 
-JCLMIDP20_EXT_DIR="${JAVAHOME}/lib/jclMidp20/ext"
-if [ ! -d ${GENERATED_VERSION} ]; then
+JCLMIDP20_EXT_DIR="${JAVA_HOME}/lib/jclMidp20/ext"
+if [ ! -d "${JCLMIDP20_EXT_DIR}" ]; then
     mkdir ${JCLMIDP20_EXT_DIR}
 fi
 
@@ -26,7 +26,7 @@ cp ${BLUECOVE_GPL_JAR} "${JCLMIDP20_EXT_DIR}/bluecove-gpl.jar"
 cp ${BLUECOVE_GPL_PROJECT_HOME}/src/main/resources/*.so "${JAVA_HOME}/bin"
 
 
-${JAVAHOME}/bin/j9 ${JVM_ARGS} -cp ${BLUECOVE_TESTER_HOME}/target/bctest.jar javax.microedition.lcdui.AppManager ${BLUECOVE_TESTER_HOME}/target/bctest.jad
+${JAVA_HOME}/bin/j9 ${JVM_ARGS} -cp ${BLUECOVE_TESTER_HOME}/target/bctest.jar javax.microedition.lcdui.AppManager ${BLUECOVE_TESTER_HOME}/target/bctest.jad
 rc=$?
 if [ ! "${rc}" = "0" ]; then
     echo Error calling java
