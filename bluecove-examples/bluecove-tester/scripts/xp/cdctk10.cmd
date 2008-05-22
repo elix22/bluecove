a3fac3e6962dc7e68d4be13e48c4e9e4cbbb26e6
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2008-05-22 15:31:05 +0000

    a way run on sun cdc

--- bluecove-examples/bluecove-tester/scripts/xp/cdctk10.cmd
+++ bluecove-examples/bluecove-tester/scripts/xp/cdctk10.cmd
@@ -9,16 +9,24 @@ if errorlevel 1 goto endmark
 
 set JVM_ARGS=
 rem set JVM_ARGS=%JVM_ARGS% -Dbluecove.debug=1
+rem set JVM_ARGS=%JVM_ARGS% -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=y
 
 title CDCTK 1.0
 
 set CP=%BLUECOVE_TESTER_HOME%\target\classes
-set CP=%CP%;%BLUECOVE_PROJECT_HOME%\target\classes
+rem set CP=%CP%;%BLUECOVE_PROJECT_HOME%\target\classes
 set CP=%CP%;%BLUECOVE_TESTER_HOME%\target\cldcunit.jar
 set CP=%CP%;%BLUECOVE_TESTER_HOME%\target\cldcunit-se.jar
 
-rem set JVM_ARGS=%JVM_ARGS% -Dbluecove.native.resource=false
-rem set JVM_ARGS=%JVM_ARGS% -Djava.library.path=%BLUECOVE_PROJECT_HOME%\src\main\resources
+
+set JVM_ARGS=%JVM_ARGS% -Dbluecove.native.resource=false
+rem set JVM_ARGS=%JVM_ARGS% -Dbluecove.native.path=%BLUECOVE_PROJECT_HOME%\src\main\resources
+set JVM_ARGS=%JVM_ARGS% -Dbluecove.native.path=%CDCTK_HOME%\bin
+
+set BOOTCLASSPATH=%CDCTK_HOME%\lib\agui-1.0.jar;%CDCTK_HOME%\lib\rt.jar;%CDCTK_HOME%\lib\jaas.jar;%CDCTK_HOME%\lib\jce.jar;%CDCTK_HOME%\lib\jsse-cdc.jar
+set BOOTCLASSPATH=%BOOTCLASSPATH%;%BLUECOVE_PROJECT_HOME%\target\classes
+
+set JVM_ARGS=%JVM_ARGS% -Xbootclasspath:%BOOTCLASSPATH%
 
 set START_ARGS=-cp %CP% %BLUECOVE_MAIN%
 rem set START_ARGS=-jar %BLUECOVE_TESTER_APP_JAR%
