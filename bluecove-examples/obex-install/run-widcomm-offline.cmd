728b85794f893beb566df271b47753f39b7192ab
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2008-06-15 19:35:39 +0000

    use proper version

--- bluecove-examples/obex-install/run-widcomm-offline.cmd
+++ bluecove-examples/obex-install/run-widcomm-offline.cmd
@@ -5,7 +5,7 @@ call %~dp0..\bluecove-tester\scripts\version.cmd
 SET STACK=widcomm
 title %STACK%-obex
 rem set JVM_ARGS=%JVM_ARGS% -Dbluecove.debug=1
-java -Dbluecove.stack=%STACK% %JVM_ARGS% -jar target\obex-install-%VERSION%-mini.jar
+java -Dbluecove.stack=%STACK% %JVM_ARGS% -jar target\obex-install-%BLUECOVE_VERSION%-mini.jar
 if errorlevel 2 (
     echo Error calling java
 )
--- bluecove-examples/obex-install/run-winsock.cmd
+++ bluecove-examples/obex-install/run-winsock.cmd
@@ -5,7 +5,7 @@ call %~dp0..\bluecove-tester\scripts\version.cmd
 SET STACK=winsock
 title %STACK%-obex
 rem set JVM_ARGS=%JVM_ARGS% -Dbluecove.debug=1
-java -Dbluecove.stack=%STACK% %JVM_ARGS% -jar target\obex-install-%VERSION%-mini.jar http://wintermute.de/wap/extended/5ud0ku.jar
+java -Dbluecove.stack=%STACK% %JVM_ARGS% -jar target\obex-install-%BLUECOVE_VERSION%-mini.jar http://wintermute.de/wap/extended/5ud0ku.jar
 if errorlevel 1 (
     echo Error calling java
     pause
